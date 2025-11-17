e66aa083bf8bd53d4c85438b31218c18df4114cc
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-11-17 15:33:50 +0300

    adding protection for saved rout only to acces by login user

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -2,8 +2,25 @@
 import { icons } from "@/constants/icons";
 import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
+import { useEffect } from "react";
+
 
 const Save = () => {
+  const { user, loading, setUser } = useAuth() as {
+    user: any;
+    loading: boolean;
+    setUser: (user: any) => void;
+  };
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) router.replace("/(auth)/login");
+    }
+  }, [user, loading]);
+
   return (
     <SafeAreaView className="bg-primary flex-1 px-10">
       <View className="flex justify-center items-center flex-1 flex-col gap-5">
